c4774268ad3201f3c52c155c3c339a6edb743657
Aman Kumar <amankarguwal0@gmail.com>
2025-03-27 07:27:14 +0530

    Add forms for Tweet creation and user registration

---
+++ blubird/tweet/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from .models import Tweet
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet
+        fields = ["content", "photo"]
+        widgets = {
+            "content": forms.Textarea(
+                attrs={"rows": 3, "placeholder": "What's happening?"}
+            ),
+        }
+
+
+class UserRegistrationForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+        widgets = {
+            "username": forms.TextInput(attrs={"placeholder": "Username"}),
+            "email": forms.EmailInput(attrs={"placeholder": "Email"}),
+            "password1": forms.PasswordInput(attrs={"placeholder": "Password"}),
+            "password2": forms.PasswordInput(attrs={"placeholder": "Confirm Password"}),
+        }
